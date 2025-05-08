ed7aeffaedcc3079aad8f6063d906a9e69091e33
Rozza2 <ramezemad20000@gmail.com>
2025-05-08 19:10:55 +0300

    asd

--- src/main/java/com/example/Market/Place/SecurityConfigurations/SecurityConfig.java
+++ src/main/java/com/example/Market/Place/SecurityConfigurations/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.Market.Place.Config;
+package com.example.Market.Place.SecurityConfigurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
